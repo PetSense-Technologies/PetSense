5bd1ac4d6c3729a894067bd0dd9fcd741cc8f00d
Leviathan19 <msnarvaez19@hotmail.com>
2026-07-08 00:26:17 -0500

    feat: :white_check_mark: Pruebas en Postman
    Se cambio parte del código parcialmente para hacer pruebas en Postman, se probó el endpoint predict del modelo, y se llegó a la conclusión de que no recibe peticiones multiples, sino que las encola, con un tiempo promedio de 0.5 seg lo cual es aceptable, ya que recibiendo 5 peticiones a la vez se demora 3 seg aprox

--- backend-ia-perros/analisis_modelo.py
+++ backend-ia-perros/analisis_modelo.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
-# Cargar tu modelo
+#Cargar modelo
 model = YOLO('best.pt')
 
-# Esto genera un resumen de la arquitectura del modelo
+#Genera un resumen de la arquitectura del modelo
 model.info()
 
-# Opcional: Si quieres ver las clases que aprendió
+#Para ver las clases que aprendió
 print("\nClases detectadas en el modelo:")
 print(model.names)
