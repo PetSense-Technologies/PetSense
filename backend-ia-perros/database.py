9f3555c8ff9e00c2ecb4cd85f576962ad259f66f
Fernando Wladimir Martínez Figueroa <134228921+Wmartinez1203@users.noreply.github.com>
2026-07-08 12:46:27 -0500

    feat: configuracion de base de datos local SQLite y agregando modelo best.pt

--- backend-ia-perros/database.py
+++ backend-ia-perros/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Conexión a postgres
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/petsense_db"
+# DATABASE_URL = "postgresql://postgres:1234@localhost:5432/petsense_db"
+DATABASE_URL = "sqlite:///./petsense.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
